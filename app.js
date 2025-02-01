f2434212b2edb868fe029da1cfbc837e5cd013f9
MingHuang1220 <syuminghuang@gmail.com>
2025-02-01 17:07:25 +0800

    爬取所有分頁資料

--- app.js
+++ app.js
@@ -1,63 +1,84 @@
-const ExcelJS = require('exceljs');
-const path = require('path');
-const fs = require('fs');
-const cheerio = require('cheerio');
-const puppeteer = require('puppeteer');
+const ExcelJS = require("exceljs")
+const path = require("path")
+const fs = require("fs")
+const cheerio = require("cheerio")
+const puppeteer = require("puppeteer")
 
-const getProductInfo = async ($)=>{
-   
-    // 取得商品資料
-    const productInfo = [];
-    $('.c-listInfoGrid__item').each((index, element) => {
-        console.log('-------------------')
-        const title = $(element).find('.c-prodInfoV2__title').text();
-        console.log('title:', title)
-        const price = $(element).find('.c-prodInfoV2__price .c-prodInfoV2__priceValue').text();
-        console.log('price:', price)
-        productInfo.push({title, price});
-        console.log('-------------------')
+// -- 引入js
+const { generateExcelFile } = require("./generateExcelFile")
 
-    });
-    return productInfo;
+const getProductInfo = async (browser, url = "https://24h.pchome.com.tw/category/DGAD08C" , callback) => {
+    try {
+        const page = await browser.newPage()
+        await page.goto(url)
+        console.log("page loaded")
+        // 取得商品資料
+        const productInfo = []
+
+
+        for(let i = 1; i < Infinity; i++){
+            // 等待特定的 JavaScript 條件為真
+            await page.waitForFunction(
+                'document.querySelectorAll(".c-listInfoGrid__item .c-prodInfoV2__text")[0]?.innerText'
+            )
+            
+            // 取得頁面的 HTML
+            const html = await page.content()
+            // 使用 cheerio 解析 HTML
+            const $ = cheerio.load(html)
+    
+            // 目標資料
+            const targetData = await callback($)
+            productInfo.push(...targetData)
+    
+            const nextPageButton = await page.$(".c-pagination__button.is-next button")
+            const isDisabled = await nextPageButton.evaluate(node => node.disabled)
+            if(!nextPageButton || isDisabled){
+                // 如果沒有下一頁按鈕或是按鈕被禁用，就跳出迴圈
+                break
+            }
+            // 點擊下一頁按鈕
+            await nextPageButton.click()
+        }
+        
+      
+        return productInfo
+    }catch(err){
+        console.log(err)
+        return []
+    }
 }
 
-(async ()=>{
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-    await page.goto('https://24h.pchome.com.tw/category/DGAD08C');
-    console.log('page loaded');
+;(async () => {
+    // return
+  const browser = await puppeteer.launch({ headless: false })
+  
+  const targetUrl = "https://24h.pchome.com.tw/category/DGAD08C"
+  // 取得商品資料
+  const productInfo1 = await getProductInfo(browser, targetUrl, async $=>{
     const data = []
-    // 等待特定的 JavaScript 條件為真
-    await page.waitForFunction('document.querySelectorAll(".c-listInfoGrid__item .c-prodInfoV2__text")[0]?.innerText');
+    $(".c-listInfoGrid__item").each((index, element) => {
+        console.log("-------------------")
+        const name = $(element).find(".c-prodInfoV2__title").text()
+        console.log("name:", name)
+        const price = $(element)
+          .find(".c-prodInfoV2__price .c-prodInfoV2__priceValue")
+          .text()
+        console.log("price:", price)
+        // productInfo.push({ title, price })
+        console.log("-------------------")
+        data.push({ name, price }) 
+    })
+    return data
+  })
+  console.log(productInfo1)
+  console.log(productInfo1.length)
 
-    // 取得頁面的 HTML
-    const html = await page.content();
-     // 使用 cheerio 解析 HTML
-     const $ = cheerio.load(html);
-    // 取得商品資料
-    const productInfo = await getProductInfo($);
-    data.push(...productInfo);
-    
-    const nextPageButton = await page.$('.c-pagination__button.is-next button');
-    // 點擊下一頁按鈕
-    await nextPageButton.click();
-
-
-
-    // -------- 第二頁
-    // 等待特定的 JavaScript 條件為真
-    await page.waitForFunction('document.querySelectorAll(".c-listInfoGrid__item .c-prodInfoV2__text")[0]?.innerText');
-    // 取得頁面的 HTML
-    const html2 = await page.content();
-     // 使用 cheerio 解析 HTML
-     const $2 = cheerio.load(html2);
-    // 取得商品資料
-    const productInfo2 = await getProductInfo($2);
-    data.push(...productInfo2);
-
-
-    // await browser.close();
-    // console.log('browser closed');
-    console.log(data);
-    console.log(data.length)
+  const columns = [
+      { header: 'Name', key: 'name', width: 50 },
+      { header: 'Price', key: 'price', width: 50 }
+  ]
+  await generateExcelFile(productInfo1, columns, 'pcHome')
+  // 成功完成後終止腳本
+  process.exit(0);
 })()
---
+++ generateExcelFile.js
@@ -0,0 +1,51 @@
+const ExcelJS = require("exceljs")
+const path = require("path")
+const fs = require("fs")
+// 生成 Excel 文件
+const generateExcelFile = async (data = [], columns = [], fileName = '')=>{
+    // 創建一個新的工作簿
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet('Users');
+  
+    // 添加標題行
+    // 假設 worksheet 已經定義並且包含 columns
+    worksheet.columns = columns;
+    worksheet.columns.forEach((column, index) => {
+        column.index = index + 1; // 索引從 1 開始
+    });
+  
+    // 添加數據行
+    data.forEach(user => {
+      worksheet.addRow(user);
+    });
+  
+    // 指定輸出文件夾和文件名
+    const outputDir = path.join(__dirname, 'output');
+    const timestamp = getTimestamp();
+    const outputPath = path.join(outputDir, `${fileName}_${timestamp}.xlsx`);
+  
+    // 確保輸出文件夾存在
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir);
+    }
+  
+    // 將工作簿寫入文件
+    await workbook.xlsx.writeFile(outputPath);
+    console.log(`Excel file has been saved to ${outputPath}`)
+  }
+
+  const getTimestamp = ()=>{
+    // 取得當前時間並格式化為 YYYYMMDDHHmmss
+    const now = new Date();
+    const timestamp = now.getFullYear().toString() +
+    (now.getMonth() + 1).toString().padStart(2, '0') +
+    now.getDate().toString().padStart(2, '0') +
+    now.getHours().toString().padStart(2, '0') +
+    now.getMinutes().toString().padStart(2, '0') +
+    now.getSeconds().toString().padStart(2, '0');
+    return timestamp
+  }
+
+  module.exports = {
+    generateExcelFile
+  }
